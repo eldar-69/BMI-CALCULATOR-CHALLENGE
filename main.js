8ef3df6bc8d2d38ef124b709f97ed4b7b9c41f7d
Eldar <eldarhalilovic27@gmail.com>
2022-03-16 13:56:36 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,21 @@
 //The first parameter should be the weight and the second should be the height.
 
 function bmiCalculator(weight, height){
-    var bmi = weight / (height * height)
-    return bmi;
+    var bmi = weight / Math.pow(height, 2);
+    return Math.round(bmi);
 }
 
 
 /* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
 
-var bmi = bmiCalculator(65, 1.8); 
-
 bmi should equal 20 when it's rounded to the nearest whole number.
 
+###The Math.pow() static method, given two arguments, base and exponent, returns baseexponent.
+
+###The Math.round() function returns the value of a number rounded to the nearest integer.
+
 */
+
+var bmi = bmiCalculator(65, 1.8); 
+
+console.log(bmi);
